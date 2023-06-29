2af7458e1833e6dc256a36ad754f8c3bc45ccd6b
koreanddinghwan <myunghwan0421@gmail.com>
2023-06-29 20:14:24 +0900

    init.sh

--- srcs/requirements/tools/init.sh
+++ srcs/requirements/tools/init.sh
@@ -1,25 +1,56 @@
+# !/bin/bash
+
+echo "Hi $USER"
+echo "init.sh start"
+echo "Are you in VM? (y/n)"
+read answer
+VOLUMEDIR=''
+
+if [ $answer = 'y' ]; then
+	echo "You are in VM"
+	VOLUMEDIR='/home/myukang/data'
+	echo "VOLUMEDIR=$VOLUMEDIR" >> ../.env
+else
+	echo "You are not in VM"
+	VOLUMEDIR="/Users/$USER/data"
+	echo "VOLUMEDIR=$VOLUMEDIR" >> ../.env
+fi
+
 # setup dirs
-if [ ! -f /home/myukang/data ]; then
-	echo "/home/myukang/data directory is not exist"
-	mkdir /home/myukang/data
-	mkdir /home/myukang/data/wordpress
-	mkdir /home/myukang/data/wordpress-db
+if [ ! -f $VOLUMEDIR ]; then
+	echo "$VOLUMEDIR directory is not exist"
+	mkdir $VOLUMEDIR 
+	mkdir $VOLUMEDIR/wordpress
+	mkdir $VOLUMEDIR/wordpress-db
 fi
 
-if [ ! -f /home/myukang/data/wordpress ]; then
+if [ ! -f $VOLUMEDIR/wordpress ]; then
 	echo "wordpress dir is not exist"
-	mkdir /home/myukang/data/wordpress
+	mkdir $VOLUMEDIR/wordpress
 fi
 
-if [ ! -f /home/myukang/data/wordpress-db ]; then
+if [ ! -f $VOLUMEDIR/wordpress-db ]; then
 	echo "wordpress-db dir is not exist"
-	mkdir /home/myukang/data/wordpress-db
+	mkdir $VOLUMEDIR/wordpress-db
 fi
 
-if [ ! -f /home/myukang/data/wordpress/index.php ]; then
+# this is for wordpress
+# if you want to change wordpress version, change this url
+# or you can change this to download site from your own
+if [ ! -f $VOLUMEDIR/wordpress/index.php ]; then
 	echo "wordpress file is not exist"
-	wget https://wordpress.org/latest.tar.gz -P /home/myukang/data/wordpress
-	tar -xvf /home/myukang/data/wordpress/latest.tar.gz -C /home/myukang/data/wordpress
+	wget https://wordpress.org/latest.tar.gz -P $VOLUMEDIR/wordpress
+	tar -xvf $VOLUMEDIR/wordpress/latest.tar.gz -C $VOLUMEDIR/wordpress
 fi
 
+#setup host file
+if [ ! -f /etc/hosts ]; then
+	echo "hosts file is not exist"
+	touch /etc/hosts
+fi
 
+#setup domain name
+echo "127.0.0.1 $DOMAIN_NAME" >> /etc/hosts
+
+#setup wp-config.php
+cp ../wordpress/conf/wp-config.php $VOLUMEDIR/wordpress/wp-config.php
