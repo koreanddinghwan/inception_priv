fbb6b0f99c5408479ea2d0f1c06aa87e101fd791
koreanddinghwan <myunghwan0421@gmail.com>
2023-07-03 22:26:54 +0900

    feat: vsftpd err.

--- srcs/requirements/bonus/ftp/tools/run-vsftpd.sh
+++ srcs/requirements/bonus/ftp/tools/run-vsftpd.sh
@@ -1,46 +1,13 @@
-#!/bin/bash
+#! /bin/bash
 
-# If no env var for FTP_USER has been specified, use 'admin':
-if [ "$FTP_USER" = "**String**" ]; then
-    export FTP_USER='admin'
-fi
+adduser $FTP_USER --disabled-password --gecos "" --home /home/$FTP_USER --shell /bin/ash
 
-# If no env var has been specified, generate a random password for FTP_USER:
-if [ "$FTP_PASS" = "**Random**" ]; then
-    export FTP_PASS=`cat /dev/urandom | tr -dc A-Z-a-z-0-9 | head -c${1:-16}`
-fi
+echo "$FTP_USER:$FTP_PASS" | chpasswd > /dev/null 
 
-# Do not log to STDOUT by default:
-if [ "$LOG_STDOUT" = "**Boolean**" ]; then
-    export LOG_STDOUT=''
-else
-    export LOG_STDOUT='Yes.'
-fi
+echo "local_root=/home/$FTP_USER" >> /etc/vsftpd/vsftpd.conf
 
-# Create home dir and update vsftpd user db:
-mkdir -p "/home/vsftpd/${FTP_USER}"
-chown -R ftp:ftp /home/vsftpd/
+chgrp -R $FTP_USER /home/$FTP_USER
+chown -R $FTP_USER /home/$FTP_USER
+chmod -R +x /home/$FTP_USER
 
-echo -e "${FTP_USER}\n${FTP_PASS}" > /etc/vsftpd/virtual_users.txt
-/usr/bin/db_load -T -t hash -f /etc/vsftpd/virtual_users.txt /etc/vsftpd/virtual_users.db
-
-# Set passive mode parameters:
-# if [ "$PASV_ADDRESS" = "**IPv4**" ]; then
-#     export PASV_ADDRESS=$(/sbin/ip route|awk '/default/ { print $3 }')
-# fi
-
-# echo "pasv_address=${PASV_ADDRESS}" >> /etc/vsftpd/vsftpd.conf
-# echo "pasv_max_port=${PASV_MAX_PORT}" >> /etc/vsftpd/vsftpd.conf
-# echo "pasv_min_port=${PASV_MIN_PORT}" >> /etc/vsftpd/vsftpd.conf
-# echo "pasv_addr_resolve=${PASV_ADDR_RESOLVE}" >> /etc/vsftpd/vsftpd.conf
-# echo "pasv_enable=${PASV_ENABLE}" >> /etc/vsftpd/vsftpd.conf
-# echo "file_open_mode=${FILE_OPEN_MODE}" >> /etc/vsftpd/vsftpd.conf
-# echo "local_umask=${LOCAL_UMASK}" >> /etc/vsftpd/vsftpd.conf
-# echo "xferlog_std_format=${XFERLOG_STD_FORMAT}" >> /etc/vsftpd/vsftpd.conf
-# echo "reverse_lookup_enable=${REVERSE_LOOKUP_ENABLE}" >> /etc/vsftpd/vsftpd.conf
-# echo "pasv_promiscuous=${PASV_PROMISCUOUS}" >> /etc/vsftpd/vsftpd.conf
-# echo "port_promiscuous=${PORT_PROMISCUOUS}" >> /etc/vsftpd/vsftpd.conf
-
-
-# Run vsftpd:
-&>/dev/null /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
+vsftpd /etc/vsftpd/vsftpd.conf
